520263eb2a9cc84fb40c6780c8a3ca3d8579fd18
roberto.manca <roberto.manca.89@gmail.com>
2020-09-20 18:28:21 +0200

    Add more options for nutrients

--- src/main/scala/com/octawizard/diet/DietGenerator.scala
+++ src/main/scala/com/octawizard/diet/DietGenerator.scala
@@ -5,9 +5,41 @@ package com.octawizard.diet
  */
 object DietGenerator extends App {
 
-  val proteins: Set[Protein] = Protein.of("chicken", "tuna", "shrimps")
-  val vegetables = Vegetable.of("broccoli", "spinach", "beetroot", "coliflower", "carrot", "tomato")
-  val carbs  = Carb.of("pasta", "rice", "bread", "potato", "sweet potato")
+  val proteins: Set[Protein] = Protein.of(
+    "chicken",
+    "tuna",
+    "shrimps",
+    "eggs",
+    "beans",
+    "chickpeas",
+    "lentil",
+    "cottage cheese",
+    "clams",
+    "cod",
+    "salmon"
+  )
+  val vegetables = Vegetable.of(
+    "broccoli",
+    "spinach",
+    "beetroot",
+    "cauliflower",
+    "eggplant",
+    "zucchini",
+    "red pepper",
+    "green beans",
+    "edamame",
+    "cabbage",
+    "pumpkin"
+  )
+  val sideVegetables = Vegetable.of(
+    "cucumber",
+    "tomato",
+    "carrot",
+    "rucola",
+    "lettuce",
+    "dried tomato"
+  )
+  val carbs = Carb.of("pasta", "brown rice", "brown bread", "potato", "sweet potato", "quinoa")
   val weeklyDiet = new WeeklyDiet(proteins, vegetables, carbs)
 
   println(weeklyDiet.toString)
