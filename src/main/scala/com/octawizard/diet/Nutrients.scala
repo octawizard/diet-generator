4b23dd5bf51d2584f398a0f19507720ecbef8248
roberto.manca <roberto.manca.89@gmail.com>
2020-09-20 18:27:34 +0200

    Refactor builders for nutrients

--- src/main/scala/com/octawizard/diet/Nutrients.scala
+++ src/main/scala/com/octawizard/diet/Nutrients.scala
@@ -1,7 +1,5 @@
 package com.octawizard.diet
 
-import scala.collection.mutable
-
 /**
  * Created by Roberto Manca on 19/09/2020.
  */
@@ -9,26 +7,25 @@ case class Protein(name: String)
 
 object Protein {
   def of(protein: String, proteins: String*): Set[Protein] = {
-    val set = mutable.Set(protein)
-    proteins.foreach(p => set.add(p))
-    set.map(Protein(_)).toSet
+    val set = proteins.toSet + protein
+    set.map(Protein(_))
   }
 }
 
 case class Vegetable(name: String)
+
 object Vegetable {
   def of(v: String, vv: String*): Set[Vegetable] = {
-    val set = mutable.Set(v)
-    vv.foreach(set.add)
-    set.map(Vegetable(_)).toSet
+    val set = vv.toSet + v
+    set.map(Vegetable(_))
   }
 }
 
 case class Carb(name: String)
+
 object Carb {
   def of(c: String, cc: String*): Set[Carb] = {
-    val set = mutable.Set(c)
-    cc.foreach(set.add)
-    set.map(Carb(_)).toSet
+    val set = cc.toSet + c
+    set.map(Carb(_))
   }
 }
